ed3591bfd8aeb19aab5440ad165e03da3345a132
Azki Rasyid <73322974+azkira@users.noreply.github.com>
2021-12-14 13:06:09 +0700

    commit

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 # app.py
-from flask import Flask           # import flask
+from flask import Flask        # import flask
 app = Flask(__name__)             # create an app instance
 
-@app.route("/")                   # at the end point /
-def hello():                      # call method hello
-    return '<h1>Hello World</h1>' # which returns "hello world"if __name__ == "__main__":        # on running python app.py
+@app.route("/get", methods=['GET'])                   # at the end point /
+def home():                      # call method hello
+  return {
+    "message": "anjing luh bre"
+  }
 
 if __name__ == '__main__':
   app.run()
